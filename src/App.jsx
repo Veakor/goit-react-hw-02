c930ac29c7ff1bd728fcf337726787679cfb1eb3
Veakor <135655247+Veakor@users.noreply.github.com>
2024-03-17 19:02:31 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
 
-
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 
-class App extends Component {
-  state = {
-    feedback: {
-      good: 0,
-      neutral: 0,
-      bad: 0
-    }
-  };
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
 
-  updateFeedback = feedbackType => {
-    this.setState(prevState => ({
-      feedback: {
-        ...prevState.feedback,
-        [feedbackType]: prevState.feedback[feedbackType] + 1
-      }
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
     }));
   };
 
-  render() {
-    const { feedback } = this.state;
-
-    return (
-      <div>
-        <h1>Sip Happens Café</h1>
-        <p>Please leave your feedback about our service by selecting one of the options below.</p>
-        <Options updateFeedback={this.updateFeedback} />
-        <Feedback feedback={feedback} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Options updateFeedback={updateFeedback} />
+      <Feedback feedback={feedback} />
+    </div>
+  );
 }
 
 export default App;
+
